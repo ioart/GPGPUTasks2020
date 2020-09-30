a65c5848ae2e720d2048620524b0f4e55b5c99f0
Artem Ioselevskii <artem.ioselevskii@gmail.com>
2020-09-30 23:02:33 +0300

    task03: Todo 3.2 sum1

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,19 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+__kernel void sum1(
+        __global const unsigned* result,
+        __global const unsigned* as,
+        unsigned n)
+{
+    const unsigned id = get_global_id(0);
+
+    if (id >= n) {
+        return;
+    }
+
+    atomic_add(result, as[id]);
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,11 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+// Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:22
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -17,22 +22,22 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
-    int benchmarkingIters = 10;
+    unsigned benchmarkingIters = 10;
 
     unsigned int reference_sum = 0;
     unsigned int n = 100*1000*1000;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(42);
-    for (int i = 0; i < n; ++i) {
+    for (unsigned i = 0; i < n; ++i) {
         as[i] = (unsigned int) r.next(std::numeric_limits<unsigned int>::max() / n);
         reference_sum += as[i];
     }
 
     {
         timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (unsigned iter = 0; iter < benchmarkingIters; ++iter) {
             unsigned int sum = 0;
-            for (int i = 0; i < n; ++i) {
+            for (unsigned i = 0; i < n; ++i) {
                 sum += as[i];
             }
             EXPECT_THE_SAME(reference_sum, sum, "CPU result should be consistent!");
@@ -44,10 +49,10 @@ int main(int argc, char **argv)
 
     {
         timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (unsigned iter = 0; iter < benchmarkingIters; ++iter) {
             unsigned int sum = 0;
             #pragma omp parallel for reduction(+:sum)
-            for (int i = 0; i < n; ++i) {
+            for (unsigned i = 0; i < n; ++i) {
                 sum += as[i];
             }
             EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
@@ -59,6 +64,38 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        unsigned result = 0;
+        auto result_gpu = gpu::gpu_mem_32u::createN(1);
+        auto as_gpu = gpu::gpu_mem_32u::createN(n);
+        as_gpu.writeN(as.data(), n);
+
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+        gpu::WorkSize work_size(workGroupSize, global_work_size);
+
+        {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum1");
+            sum.compile();
+
+            timer t;
+            for (unsigned iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned empty = 0;
+                result_gpu.writeN(&empty, 1);
+                sum.exec(work_size, result_gpu, as_gpu, n);
+
+                result_gpu.readN(&result, 1);
+                EXPECT_THE_SAME(reference_sum, result, "GPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU:     sum1" << std::endl;
+            std::cout << "GPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU:     " << n / 1e6 / t.lapAvg() << " millions/s" << std::endl;
+        }
+
     }
 }
